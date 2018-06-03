bf34953d3e35d0e488fa89ca037ff2f17589e23a
vikrame <vikrame@lymbyc.com>
2018-06-04 00:37:38 +0530

    added file destination

--- App.py
+++ App.py
@@ -1,13 +1,18 @@
 from flask import Flask, request
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
 
+photos = UploadSet('photos', IMAGES)
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
+configure_uploads(app, photos)
 
 @app.route('/upload', methods=['POST'])
 def upload():
     print('hi')
     name = request.form['name']
     regno = request.form['regNo']
+    filename = photos.save(request.files['timetable'])
     print(name, regno)
     return 'Uploaded successfully'
 
