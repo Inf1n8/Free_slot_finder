e03310a07d0e3d0e060f56df168d35454cd0dd03
vikrame <vikrame@lymbyc.com>
2018-06-04 00:29:19 +0530

    modified app.py

--- App.py
+++ App.py
@@ -8,8 +8,18 @@ def upload():
     print('hi')
     name = request.form['name']
     regno = request.form['regNo']
-    print(name, regno,request)
+    print(name, regno)
     return 'Uploaded successfully'
+
+
+@app.route('/findMembers', methods=['POST'])
+def findMembers():
+    print(request.json['day'], request.json['tabId'])
+    day = request.json['day']
+    tabId = request.json['tabId']
+    return 'recieved info successfully'
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
